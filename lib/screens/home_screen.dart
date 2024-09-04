585d8b8115b17527067d5323d8f13cb35a7087ae
VoHoangKiet <vohoangkiet.dev2510@gmail.com>
2024-09-04 18:03:29 +0700

    feat: add tabbar screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -165,97 +165,115 @@ class _HomeScreenState extends State<HomeScreen> {
 class CustomTabBar extends StatelessWidget {
   const CustomTabBar({super.key});
   static double tabWidth = 100.0;
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
+        margin: EdgeInsets.only(
+          left: UtilSize.responsiveFontSize(context, 24),
+          right: UtilSize.responsiveFontSize(context, 24),
+        ),
         child: DefaultTabController(
           length: 4,
           child: Scaffold(
             appBar: AppBar(
-              bottom: TabBar(
-                tabAlignment: TabAlignment.start,
-                isScrollable: true,
-                unselectedLabelColor: Colors.white.withOpacity(0.3),
-                indicatorColor: const Color(0xFFFA4A0C),
-                labelPadding: const EdgeInsets.symmetric(horizontal: 16.0),
-                tabs: [
-                  Tab(
-                    child: SizedBox(
-                      width: tabWidth,
-                      child: Center(
-                        child: Text(
-                          "Foods",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'SF-PRO',
-                            fontWeight: FontWeight.normal,
-                            fontSize: UtilSize.responsiveFontSize(context, 14),
-                            height: 0.9,
+              toolbarHeight: 0,
+              elevation: 0,
+              backgroundColor: Color(0xFFF2F2F2),
+              bottom: PreferredSize(
+                preferredSize:
+                    const Size.fromHeight(50.0), // Adjust height as needed
+                child: Container(
+                  child: TabBar(
+                    tabAlignment: TabAlignment.start,
+                    isScrollable: true,
+                    labelColor: Color(0xFFFA4A0C), // Color for selected tab
+                    unselectedLabelColor:
+                        Color(0xFF9A9A9D), // Color for unselected tabs
+                    indicatorColor: const Color(0xFFFA4A0C),
+                    labelPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    dividerColor: Colors.transparent,
+                    tabs: [
+                      Tab(
+                        child: SizedBox(
+                          width: tabWidth,
+                          child: Center(
+                            child: Text(
+                              "Foods",
+                              style: TextStyle(
+                                fontFamily: 'SF-PRO',
+                                fontWeight: FontWeight.w300,
+                                fontSize:
+                                    UtilSize.responsiveFontSize(context, 14),
+                                height: 0.9,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  Tab(
-                    child: SizedBox(
-                      width: tabWidth,
-                      child: Center(
-                        child: Text(
-                          "Drinks",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'SF-PRO',
-                            fontWeight: FontWeight.normal,
-                            fontSize: UtilSize.responsiveFontSize(context, 14),
-                            height: 0.9,
+                      Tab(
+                        child: SizedBox(
+                          width: tabWidth,
+                          child: Center(
+                            child: Text(
+                              "Drinks",
+                              style: TextStyle(
+                                fontFamily: 'SF-PRO',
+                                fontWeight: FontWeight.w300,
+                                fontSize:
+                                    UtilSize.responsiveFontSize(context, 14),
+                                height: 0.9,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  Tab(
-                    child: SizedBox(
-                      width: tabWidth,
-                      child: Center(
-                        child: Text(
-                          "Snacks",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'SF-PRO',
-                            fontWeight: FontWeight.normal,
-                            fontSize: UtilSize.responsiveFontSize(context, 14),
-                            height: 0.9,
+                      Tab(
+                        child: SizedBox(
+                          width: tabWidth,
+                          child: Center(
+                            child: Text(
+                              "Snacks",
+                              style: TextStyle(
+                                fontFamily: 'SF-PRO',
+                                fontWeight: FontWeight.w300,
+                                fontSize:
+                                    UtilSize.responsiveFontSize(context, 14),
+                                height: 0.9,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  Tab(
-                    child: SizedBox(
-                      width: tabWidth,
-                      child: Center(
-                        child: Text(
-                          "Sauce",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'SF-PRO',
-                            fontWeight: FontWeight.normal,
-                            fontSize: UtilSize.responsiveFontSize(context, 14),
-                            height: 0.9,
+                      Tab(
+                        child: SizedBox(
+                          width: tabWidth,
+                          child: Center(
+                            child: Text(
+                              "Sauce",
+                              style: TextStyle(
+                                fontFamily: 'SF-PRO',
+                                fontWeight: FontWeight.w300,
+                                fontSize:
+                                    UtilSize.responsiveFontSize(context, 14),
+                                height: 0.9,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-            body: TabBarView(
+            body: const TabBarView(
               children: [
-                Icon(Icons.flight, size: 100),
-                Icon(Icons.directions_transit, size: 100),
-                Icon(Icons.directions_car, size: 350),
+                Center(child: Icon(Icons.flight, size: 100)),
+                Center(child: Icon(Icons.directions_transit, size: 100)),
+                Center(child: Icon(Icons.directions_car, size: 100)),
+                Center(child: Icon(Icons.directions_car, size: 100)),
               ],
             ),
           ),
