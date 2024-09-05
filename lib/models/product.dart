a82acc2927d6a41af12cbe16754f4a49be32a995
VoHoangKiet <vohoangkiet.dev2510@gmail.com>
2024-09-05 22:19:05 +0700

    feat: implement API search products.

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,9 +3,14 @@ class Product {
   final String name;
   final String price;
 
-  Product({
-    required this.imagePath,
-    required this.name,
-    required this.price,
-  });
+  Product({required this.imagePath, required this.name, required this.price});
+
+  // Hàm chuyển từ JSON thành đối tượng Product
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      imagePath: json['imagePath'],
+      name: json['name'],
+      price: json['price'],
+    );
+  }
 }
--- lib/screens/search_result_screen.dart
+++ lib/screens/search_result_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:orderfood1/services/product_services.dart';
 import 'package:orderfood1/utils/message_util.dart';
 
 import '../models/product.dart';
 import '../widgets/rounded_image.dart';
+
+class SearchResultBody extends StatefulWidget {
+  const SearchResultBody({super.key});
+
+  @override
+  State<SearchResultBody> createState() => _SearchResultBodyState();
+}
 
 class SearchResultScreen extends StatefulWidget {
   final String initialSearch;
@@ -14,202 +22,6 @@ class SearchResultScreen extends StatefulWidget {
 
   @override
   State<SearchResultScreen> createState() => _SearchResultScreenState();
-}
-
-class _SearchResultScreenState extends State<SearchResultScreen> {
-  late TextEditingController _searchController;
-  @override
-  void initState() {
-    super.initState();
-    _searchController = TextEditingController(text: widget.initialSearch);
-  }
-
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color(0xFFEEEEEE),
-        appBar: AppBar(
-          backgroundColor: const Color(0xFFEEEEEE),
-          title: TextFormField(
-            controller: _searchController,
-            textAlign: TextAlign.start,
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-            ),
-            style: TextStyle(
-              color: Colors.black.withOpacity(0.9),
-              fontFamily: "SF-PRO",
-              fontSize: 18,
-            ),
-          ),
-          centerTitle: true,
-          leading: IconButton(
-            icon: SvgPicture.asset("assets/icons/back-button.svg"),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        body: const SearchResultBody(),
-      ),
-    );
-  }
-}
-
-class SearchResultBody extends StatefulWidget {
-  const SearchResultBody({super.key});
-
-  @override
-  State<SearchResultBody> createState() => _SearchResultBodyState();
-}
-
-class _SearchResultBodyState extends State<SearchResultBody> {
-  late List<Product> products = [
-    Product(
-      imagePath: "assets/images/image_21.png",
-      name: "Veggie Tomato Mix",
-      price: "N1,900",
-    ),
-    Product(
-      imagePath: "assets/images/image_22.png",
-      name: "Organic Carrot Blend",
-      price: "N2,200",
-    ),
-    Product(
-      imagePath: "assets/images/image_23.png",
-      name: "Fresh Spinach Pack",
-      price: "N1,500",
-    ),
-    Product(
-      imagePath: "assets/images/image_24.png",
-      name: "Herbal Lettuce",
-      price: "N1,800",
-    ),
-    Product(
-      imagePath: "assets/images/image_24.png",
-      name: "Green Pepper Mix",
-      price: "N2,000",
-    ),
-    Product(
-      imagePath: "assets/images/image_22.png",
-      name: "Tomato Basil Combo",
-      price: "N2,100",
-    ),
-  ];
-
-  Future<void> _refresh() async {
-    // Simulate a network call or data fetching
-    await Future.delayed(const Duration(seconds: 2));
-
-    // Create a new list of products (simulate updated data)
-    final List<Product> updatedProducts = [
-      Product(
-        imagePath: "assets/images/image_21.png",
-        name: "Sweet Corn",
-        price: "N2,300",
-      ),
-      Product(
-        imagePath: "assets/images/image_22.png",
-        name: "Red Bell Pepper",
-        price: "N2,100",
-      ),
-      Product(
-        imagePath: "assets/images/image_23.png",
-        name: "Arugula Leaves",
-        price: "N1,700",
-      ),
-      Product(
-        imagePath: "assets/images/image_24.png",
-        name: "Zucchini",
-        price: "N1,600",
-      ),
-      Product(
-        imagePath: "assets/images/image_24.png",
-        name: "Cherry Tomatoes",
-        price: "N2,200",
-      ),
-      Product(
-        imagePath: "assets/images/image_23.png",
-        name: "Baby Spinach",
-        price: "N1,800",
-      ),
-    ];
-
-    setState(() {
-      products.clear();
-      products.addAll(updatedProducts);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: _refresh,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 25,
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30.0),
-                ),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(25),
-                    child: Text(
-                      "Found 6 results",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: "SF-PRO",
-                          fontSize: 30,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                  Expanded(
-                    child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: 0.5,
-                        mainAxisSpacing: 10,
-                        crossAxisSpacing: 10,
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      itemCount: products.length,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () =>
-                              {MessageUtil.showToast(products[index].name)},
-                          child: TabItem(product: products[index]),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 45,
-          )
-        ],
-      ),
-    );
-  }
 }
 
 class TabItem extends StatelessWidget {
@@ -296,5 +108,150 @@ class TabItem extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class _SearchResultBodyState extends State<SearchResultBody> {
+  late Future<List<Product>> futureProducts;
+  @override
+  void initState() {
+    super.initState();
+    futureProducts = ProductServices().fetchProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: futureProducts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No products found'));
+          } else {
+            final products = snapshot.data!;
+            return RefreshIndicator(
+              onRefresh: _refresh,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30.0),
+                        ),
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(25),
+                            child: Text(
+                              "Found ${products.length} results",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: "SF-PRO",
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          Expanded(
+                            child: GridView.builder(
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                childAspectRatio: 0.5,
+                                mainAxisSpacing: 10,
+                                crossAxisSpacing: 10,
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              itemCount: products.length,
+                              itemBuilder: (context, index) {
+                                return GestureDetector(
+                                  onTap: () => {
+                                    MessageUtil.showToast(products[index].name)
+                                  },
+                                  child: TabItem(product: products[index]),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 45,
+                  )
+                ],
+              ),
+            );
+          }
+        });
+  }
+
+  Future<void> _refresh() async {
+    try { 
+      List<Product> updatedProducts = await ProductServices().fetchProducts();
+      setState(() {
+        futureProducts = Future.value(updatedProducts);
+      });
+      MessageUtil.showToast("Reloaded successfully"); 
+    } catch (e) {
+      MessageUtil.showToast("Failed to reload");
+    }
+  }
+}
+
+class _SearchResultScreenState extends State<SearchResultScreen> {
+  late TextEditingController _searchController;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xFFEEEEEE),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFEEEEEE),
+          title: TextFormField(
+            controller: _searchController,
+            textAlign: TextAlign.start,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+            ),
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.9),
+              fontFamily: "SF-PRO",
+              fontSize: 18,
+            ),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: SvgPicture.asset("assets/icons/back-button.svg"),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: const SearchResultBody(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController = TextEditingController(text: widget.initialSearch);
   }
 }
